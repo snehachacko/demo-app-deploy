1fd2e5687888f2125c89a37e935534e593fc6a07
Sneha Chacko <snehachacko@users.noreply.github.com>
2020-09-12 14:23:18 +0530

    Updates

---
+++ precache-manifest.259b5af29ec20a598713df97445b39ec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "094101e187a932be799888936c67bc83",
+    "url": "/demo-app-deploy/index.html"
+  },
+  {
+    "revision": "26add1f8be23ac4cdf51",
+    "url": "/demo-app-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "16fc3a50919d4e21610b",
+    "url": "/demo-app-deploy/static/js/2.8a343e44.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/demo-app-deploy/static/js/2.8a343e44.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26add1f8be23ac4cdf51",
+    "url": "/demo-app-deploy/static/js/main.9975fda4.chunk.js"
+  },
+  {
+    "revision": "603c379d09702a880fbc",
+    "url": "/demo-app-deploy/static/js/runtime-main.97b99f1e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
